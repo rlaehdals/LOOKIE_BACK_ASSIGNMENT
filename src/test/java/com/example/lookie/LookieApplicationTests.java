1e4d47b31793c2f3c7d9a8574557292ff5c1b790
myunbongs <myunbongs@naver.com>
2022-04-06 17:02:24 +0900

    group entity

---
+++ src/test/java/com/example/lookie/LookieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
