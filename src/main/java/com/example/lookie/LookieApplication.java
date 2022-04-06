8f793f6d8c55ce2c6ce93fd878685aaf70ab94cc
rlaehdals <rkdlem48@gmail.com>
2022-04-06 15:41:36 +0900

    init

---
+++ src/main/java/com/example/lookie/LookieApplication.java
@@ -0,0 +1,13 @@
+package com.example.lookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookieApplication.class, args);
+	}
+
+}
