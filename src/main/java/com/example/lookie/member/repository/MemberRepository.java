3a7f24fed6f2f011db1f54f8a5c30e13566c30a6
rlaehdals <rkdlem48@gmail.com>
2022-05-04 17:16:45 +0900

    feat memberRepository findByEmail query

--- src/main/java/com/example/lookie/member/repository/MemberRepository.java
+++ src/main/java/com/example/lookie/member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.example.lookie.member.repository;
 import com.example.lookie.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    Optional<Member> findByEmail(String email);
 }
