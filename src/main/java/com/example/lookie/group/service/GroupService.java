0427f67c447838a0d9602ec72eca9f35b2ca8743
rlaehdals <rkdlem48@gmail.com>
2022-05-04 17:59:42 +0900

    feat Group 비즈니스 로직 추가 생성 조회 삭제

---
+++ src/main/java/com/example/lookie/group/service/GroupService.java
@@ -0,0 +1,23 @@
+package com.example.lookie.group.service;
+
+import com.example.lookie.group.domain.Group;
+import com.example.lookie.question.domain.Question;
+
+import java.util.List;
+
+public interface GroupService {
+
+    void createGroup(String name, String description, String ownerEmail);
+
+    Group findByNameGroup(String name);
+
+    List<Group> findAllGroup();
+
+    void deleteGroup(String name);
+
+    void changeGroupName(String changeName, String ownerEmail);
+
+    void changeDescription(String changeDescription, String ownerEmail);
+
+    List<Question> findQuestionList(String name);
+}
