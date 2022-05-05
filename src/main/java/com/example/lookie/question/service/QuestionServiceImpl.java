789d6f11775c91fe5f9d8b1d90af548728f5b983
rlaehdals <rkdlem48@gmail.com>
2022-05-06 01:50:48 +0900

    fix exception message

--- src/main/java/com/example/lookie/question/service/QuestionServiceImpl.java
+++ src/main/java/com/example/lookie/question/service/QuestionServiceImpl.java
@@ -17,7 +17,7 @@ public class QuestionServiceImpl implements QuestionService{
     @Override
     public void addQuestion(String ownerEmail, String title) {
         Group group = groupRepository.findByOwnerEmail(ownerEmail).orElseThrow(() -> {
-            throw new IllegalArgumentException("없는 그룹의 이름입니다.");
+            throw new IllegalArgumentException("그룹을 만드시지 않았습니다. ");
         });
 
         Question question = Question.createQuestion(group, title);
