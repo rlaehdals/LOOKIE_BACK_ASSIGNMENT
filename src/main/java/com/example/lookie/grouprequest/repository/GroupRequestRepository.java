1ea5591cdad5052a73f24e0a7cce985adbceffd9
rlaehdals <rkdlem48@gmail.com>
2022-05-19 16:18:01 +0900

    feat GroupRequestRepository Test

--- src/main/java/com/example/lookie/grouprequest/repository/GroupRequestRepository.java
+++ src/main/java/com/example/lookie/grouprequest/repository/GroupRequestRepository.java
@@ -4,6 +4,7 @@ import com.example.lookie.grouprequest.domain.GroupRequest;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +12,7 @@ import java.util.Optional;
 public interface GroupRequestRepository extends JpaRepository<GroupRequest, Long> {
 
     @Query("select gr from GroupRequest gr join fetch gr.member join fetch gr.group where gr.member.email=:memberEmail and gr.group.name=:groupName")
-    Optional<GroupRequest> findByMemberEmailAndGroupName(String memberEmail, String groupName);
+    Optional<GroupRequest> findByMemberEmailAndGroupName(@Param("memberEmail") String memberEmail, @Param("groupName") String groupName);
 
 //    @EntityGraph(attributePaths = "group")
     @Query("select gr from GroupRequest gr join fetch gr.group where gr.id=:id")
--- src/test/java/com/example/lookie/grouprequest/repository/GroupRequestRepositoryTest.java
+++ src/test/java/com/example/lookie/grouprequest/repository/GroupRequestRepositoryTest.java
@@ -4,6 +4,7 @@ import com.example.lookie.group.domain.Group;
 import com.example.lookie.group.repository.GroupRepository;
 import com.example.lookie.grouprequest.domain.Department;
 import com.example.lookie.grouprequest.domain.GroupRequest;
+import com.example.lookie.grouprequest.domain.RequestStatus;
 import com.example.lookie.member.domain.Address;
 import com.example.lookie.member.domain.Member;
 import com.example.lookie.member.repository.MemberRepository;
@@ -37,7 +38,8 @@ class GroupRequestRepositoryTest {
         /*
         given
         */
-        Member userMember = Member.createUserMember("abc@abc.com", "1234", "ABC", new Address("서울", "종로", "000006"));
+        Member userMember = Member.createUserMember("abc@abc.com", "1234", "ABC"
+                , new Address("서울", "종로", "000006"));
 
         Member userResult = memberRepository.save(userMember);
 
@@ -48,7 +50,8 @@ class GroupRequestRepositoryTest {
         List<QuestionAnswer> questionAnswerList = new ArrayList<>();
         QuestionAnswer[] EMPTY = new QuestionAnswer[0];
 
-        GroupRequest groupRequest = GroupRequest.createGroupRequest(userResult, groupResult, department, questionAnswerList.toArray(EMPTY));
+        GroupRequest groupRequest = GroupRequest.createGroupRequest(userResult, groupResult, department
+                , questionAnswerList.toArray(EMPTY));
 
         /*
         when
@@ -67,10 +70,13 @@ class GroupRequestRepositoryTest {
         /*
         given
          */
-        Member userMember1 = Member.createUserMember("abc@abc.com", "1234", "ABC", new Address("서울", "종로", "000006"));
-        Member userMember2 = Member.createUserMember("hij@hij.com", "1234", "HIJ", new Address("서울", "종로", "000006"));
+        Member userMember1 = Member.createUserMember("abc@abc.com", "1234", "ABC",
+                new Address("서울", "종로", "000006"));
+        Member userMember2 = Member.createUserMember("hij@hij.com", "1234", "HIJ",
+                new Address("서울", "종로", "000006"));
 
-        Member adminMember = Member.createAdminMember("efg@efg.com", "1234", "EFG", new Address("서울", "종로", "000006"));
+        Member adminMember = Member.createAdminMember("efg@efg.com", "1234", "EFG",
+                new Address("서울", "종로", "000006"));
 
         Member userResult1 = memberRepository.save(userMember1);
         Member userResult2 = memberRepository.save(userMember2);
@@ -84,8 +90,10 @@ class GroupRequestRepositoryTest {
         List<QuestionAnswer> questionAnswerList = new ArrayList<>();
         QuestionAnswer[] EMPTY = new QuestionAnswer[0];
 
-        GroupRequest groupRequest1 = GroupRequest.createGroupRequest(userResult1, groupResult, department, questionAnswerList.toArray(EMPTY));
-        GroupRequest groupRequest2 = GroupRequest.createGroupRequest(userResult2, groupResult, department, questionAnswerList.toArray(EMPTY));
+        GroupRequest groupRequest1 = GroupRequest.createGroupRequest(userResult1, groupResult,
+                department, questionAnswerList.toArray(EMPTY));
+        GroupRequest groupRequest2 = GroupRequest.createGroupRequest(userResult2, groupResult,
+                department, questionAnswerList.toArray(EMPTY));
 
         groupRequestRepository.save(groupRequest1);
         groupRequestRepository.save(groupRequest2);
@@ -101,6 +109,54 @@ class GroupRequestRepositoryTest {
         assertThat(result.size()).isEqualTo(2);
     }
 
+    @Test
+    @DisplayName("그룹 신청 변경 테스트 ")
+    void changeGroupRequestStatus() {
+        /*
+        given
+         */
+        Member userMember1 = Member.createUserMember("abc@abc.com", "1234", "ABC",
+                new Address("서울", "종로", "000006"));
+        Member userMember2 = Member.createUserMember("hij@hij.com", "1234", "HIJ",
+                new Address("서울", "종로", "000006"));
 
+        Member adminMember = Member.createAdminMember("efg@efg.com", "1234", "EFG",
+                new Address("서울", "종로", "000006"));
 
+        Member userResult1 = memberRepository.save(userMember1);
+        Member userResult2 = memberRepository.save(userMember2);
+
+        Member adminResult = memberRepository.save(adminMember);
+
+        Group group = Group.createGroup("루키", "루키입니다", adminResult.getEmail());
+        Group groupResult = groupRepository.save(group);
+
+        Department department = FRONT_END;
+        List<QuestionAnswer> questionAnswerList = new ArrayList<>();
+        QuestionAnswer[] EMPTY = new QuestionAnswer[0];
+
+        GroupRequest groupRequest1 = GroupRequest.createGroupRequest(userResult1, groupResult,
+                department, questionAnswerList.toArray(EMPTY));
+        GroupRequest groupRequest2 = GroupRequest.createGroupRequest(userResult2, groupResult,
+                department, questionAnswerList.toArray(EMPTY));
+
+        groupRequestRepository.save(groupRequest1);
+        groupRequestRepository.save(groupRequest2);
+
+        /*
+        when
+         */
+
+        GroupRequest findGroupRequest = groupRequestRepository.findByMemberEmailAndGroupName(userMember1.getEmail(),
+                group.getName()).get();
+
+        findGroupRequest.changeRequestStatus(RequestStatus.ACCEPT);
+
+        GroupRequest groupRequest = groupRequestRepository.findByMemberEmailAndGroupName(userMember1.getEmail(),
+                group.getName()).get();
+        /*
+        then
+         */
+        assertThat(groupRequest.getRequestStatus()).isEqualTo(RequestStatus.ACCEPT);
+    }
 }
