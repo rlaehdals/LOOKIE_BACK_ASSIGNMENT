9552ac38752172fe21413e5d5560dc0c4ca20944
rlaehdals <rkdlem48@gmail.com>
2022-05-19 16:04:14 +0900

    feat QuestionRepository Test

--- src/test/java/com/example/lookie/question/repository/QuestionRepositoryTest.java
+++ src/test/java/com/example/lookie/question/repository/QuestionRepositoryTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -76,4 +77,28 @@ public class QuestionRepositoryTest {
         assertThat(result.size()).isEqualTo(2);
     }
 
+    @Test
+    @DisplayName("질문 타이틀 변경 테스트")
+    void changeQuestionTitle() {
+        /*
+        given
+         */
+        Group group = Group.createGroup("루키", "루키입니다", "abc@abc.com");
+        groupRepository.save(group);
+
+        Question question = Question.createQuestion(group, "질문1");
+
+        /*
+        when
+         */
+        Question saveQuestion = questionRepository.save(question);
+
+        Question findQuestion = questionRepository.findById(saveQuestion.getId()).get();
+
+        findQuestion.changeTitle("질문2");
+
+        Question result = questionRepository.findById(saveQuestion.getId()).get();
+
+        assertThat(result.getTitle()).isEqualTo("질문2");
+    }
 }
